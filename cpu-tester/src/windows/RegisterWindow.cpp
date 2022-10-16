3e832806fc0fc7d8d611a882a3c61c5d97397087
GarondEisenfaust <chr.schr@web.de>
2022-10-16 12:58:42 +0200

    modifies the register window

--- cpu-tester/src/windows/RegisterWindow.cpp
+++ cpu-tester/src/windows/RegisterWindow.cpp
@@ -7,10 +7,13 @@ RegisterWindow::RegisterWindow(const char* title, Processor6502& processor6502)
     : GuiWindow(title), mProcessor6502(processor6502) {}
 
 void RegisterWindow::Content() {
-  ImGui::Text(fmt::format("accumulator:       {}", mProcessor6502.a).c_str());
-  ImGui::Text(fmt::format("x:                 {}", mProcessor6502.x).c_str());
-  ImGui::Text(fmt::format("y:                 {}", mProcessor6502.y).c_str());
-  ImGui::Text(fmt::format("status:            {}", mProcessor6502.status).c_str());
-  ImGui::Text(fmt::format("program counter:   {}", mProcessor6502.pc).c_str());
-  ImGui::Text(fmt::format("stack pointer:     {}", mProcessor6502.stackPointer).c_str());
+  mProcessor6502.status = 8;
+  ImGui::Text(fmt::format("status:           NVUBDIZC", mProcessor6502.status).c_str());
+  ImGui::Text(fmt::format("                  {:0>8b}", mProcessor6502.status).c_str());
+  ImGui::Text("");
+  ImGui::Text(fmt::format("program counter:  0x{:0>2x}", mProcessor6502.pc).c_str());
+  ImGui::Text(fmt::format("stack pointer:    0x{:0>2x}", mProcessor6502.stackPointer).c_str());
+  ImGui::Text(fmt::format("accumulator:      0x{:0>2x}", mProcessor6502.a).c_str());
+  ImGui::Text(fmt::format("x:                0x{:0>2x}", mProcessor6502.x).c_str());
+  ImGui::Text(fmt::format("y:                0x{:0>2x}", mProcessor6502.y).c_str());
 }
