27643d6eb93c4c214792dcc3079943879a54e304
GarondEisenfaust <chr.schr@web.de>
2022-10-16 20:45:00 +0200

    implements Cartridge

--- processor-6502/include/Cartridge.h
+++ processor-6502/include/Cartridge.h
@@ -1,20 +1,36 @@
 #pragma once
+#include "mapper/IMapper.h"
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 class Cartridge {
  public:
-  Cartridge();
-  ~Cartridge() = default;
+  struct ReadResult {
+    uint8_t readResult;
+    bool hasRead;
+  };
 
-  uint8_t Read(uint16_t addr);
-  void Write(uint16_t addr, uint8_t data);
+  enum MIRROR {
+    HORIZONTAL,
+    VERTICAL,
+  };
+
+  Cartridge(const std::string& path);
+  virtual ~Cartridge() = default;
+
+  ReadResult Read(uint16_t address);
+  bool Write(uint16_t address, uint8_t data);
 
  private:
   std::vector<uint8_t> mProgramMemory;
-  std::vector<uint8_t> mPCharacterMemory;
+  std::vector<uint8_t> mCharacterMemory;
 
   uint8_t mMapperId;
-  uint8_t ProgramBanks;
+  uint8_t mProgramBanks;
   uint8_t mCharacterBanks;
+  bool mImageValid;
+  MIRROR mMirror;
+
+  std::unique_ptr<IMapper> mMapper;
 };
--- processor-6502/src/Cartridge.cpp
+++ processor-6502/src/Cartridge.cpp
@@ -1,4 +1,72 @@
 #include "Cartridge.h"
-uint8_t Cartridge::Read(uint16_t addr) {}
+#include "mapper/Mappers.h"
+#include <fstream>
 
-void Cartridge::Write(uint16_t addr, uint8_t data) {}
+struct Header {
+  char name[4];
+  uint8_t programRomChunks;
+  uint8_t characterRomChunks;
+  uint8_t mapper1;
+  uint8_t mapper2;
+  uint8_t programRamSize;
+  uint8_t tvSystem1;
+  uint8_t tvSystem2;
+  char unused[5];
+};
+
+uint8_t DetermineMapperId(const Header& header) { return ((header.mapper2 >> 4) << 4) | (header.mapper1 >> 4); }
+
+Cartridge::MIRROR DetermineMirror(const Header& header) {
+  return (header.mapper1 & 0x01) ? Cartridge::VERTICAL : Cartridge::HORIZONTAL;
+}
+
+Cartridge::Cartridge(const std::string& path) {
+  Header header;
+
+  mImageValid = false;
+  std::ifstream romStream;
+
+  romStream.open(path, std::ifstream ::binary);
+
+  if (!romStream.is_open()) {
+    return;
+  }
+
+  romStream.read((char*)&header, sizeof(Header));
+  if (header.mapper1 & 0x04) {
+    romStream.seekg(512, std::ios_base::cur);
+  }
+
+  mMapperId = DetermineMapperId(header);
+  mMirror = DetermineMirror(header);
+
+  mProgramBanks = header.programRomChunks;
+  mProgramMemory.resize(mProgramBanks * 16384);
+  romStream.read(reinterpret_cast<char*>(mProgramMemory.data()), mProgramMemory.size());
+
+  mCharacterBanks = header.characterRomChunks;
+  mCharacterMemory.resize(mCharacterBanks * 8192);
+  romStream.read(reinterpret_cast<char*>(mCharacterMemory.data()), mCharacterMemory.size());
+
+  mMapper = std::make_unique<Mapper000>();
+
+  mImageValid = true;
+  romStream.close();
+}
+
+Cartridge::ReadResult Cartridge::Read(uint16_t address) {
+  auto mappingResult = mMapper->MapRead(address);
+  if (mappingResult.mapped) {
+    return {mProgramMemory[mappingResult.mappedAddress], true};
+  }
+  return {0, false};
+}
+
+bool Cartridge::Write(uint16_t address, uint8_t data) {
+  auto mappingResult = mMapper->MapRead(address);
+  if (mappingResult.mapped) {
+    mProgramMemory[mappingResult.mappedAddress] = data;
+    return true;
+  }
+  return false;
+}
