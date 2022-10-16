84730a62803497abc5d57c11c0b86a2d21114eb8
GarondEisenfaust <chr.schr@web.de>
2022-10-16 20:57:48 +0200

    processor now reads/writes from the cartridge

--- processor-6502/src/Bus.cpp
+++ processor-6502/src/Bus.cpp
@@ -10,12 +10,19 @@ Bus::Bus() {
 Bus::~Bus() {}
 
 void Bus::Write(uint16_t addr, uint8_t data) {
+  if (mCartridge->Write(addr, data)) {
+    return;
+  }
   if (addr >= RAM_START && addr <= RAM_END) {
     ram->at(addr % RAM_SIZE) = data;
+    return;
   }
 }
 
 uint8_t Bus::Read(uint16_t addr, bool bReadOnly) {
+  if (mCartridge->Read(addr).hasRead) {
+    return;
+  }
   if (addr >= RAM_START && addr <= RAM_END) {
     return ram->at(addr % RAM_SIZE);
   }
