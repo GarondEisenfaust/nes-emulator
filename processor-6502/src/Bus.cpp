764be5c115c9b0ba6c1ec805fb007e103963918d
GarondEisenfaust <chr.schr@web.de>
2022-10-19 16:30:11 +0200

    fixes build

--- processor-6502/src/Bus.cpp
+++ processor-6502/src/Bus.cpp
@@ -21,7 +21,7 @@ void Bus::Write(uint16_t addr, uint8_t data) {
 
 uint8_t Bus::Read(uint16_t addr, bool bReadOnly) {
   if (mCartridge->Read(addr).hasRead) {
-    return;
+    return UNDEFINED;
   }
   if (addr >= RAM_START && addr <= RAM_END) {
     return ram->at(addr % RAM_SIZE);
