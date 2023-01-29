da2fcc2971f4c4957d74021cc5bd6cb12a13fa2e
GarondEisenfaust <chr.schr@web.de>
2023-01-29 13:53:20 +0100

    adds miniaudio to the dependencies

--- conanfile.py
+++ conanfile.py
@@ -16,6 +16,7 @@ class CMakeTemplateConan(ConanFile):
     self.requires("boost/1.80.0", private=True, override=False)
     self.requires("glm/0.9.9.8", private=True, override=False)
     self.requires("lodepng/cci.20200615", private=True, override=False)
+    self.requires("miniaudio/0.11.11", private=True, override=False)
 
   def build_requirements(self):
     pass
--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,1 +1,66 @@
-int main() { return 0; }
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define MINIAUDIO_IMPLEMENTATION
+#include "miniaudio.h"
+#include <cmath>
+#include <stdio.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+
+void main_loop__em() {}
+#endif
+
+#define DEVICE_FORMAT ma_format_f32
+#define DEVICE_CHANNELS 1
+#define DEVICE_SAMPLE_RATE 44100
+
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+  ma_waveform* pSineWave;
+
+  MA_ASSERT(pDevice->playback.channels == DEVICE_CHANNELS);
+
+  pSineWave = (ma_waveform*)pDevice->pUserData;
+  MA_ASSERT(pSineWave != NULL);
+
+  ma_waveform_read_pcm_frames(pSineWave, pOutput, frameCount, NULL);
+
+  (void)pInput; /* Unused. */
+}
+
+int main(int argc, char** argv) {
+  ma_device_config deviceConfig;
+  ma_device device;
+
+  deviceConfig = ma_device_config_init(ma_device_type_playback);
+  deviceConfig.playback.format = DEVICE_FORMAT;
+  deviceConfig.playback.channels = DEVICE_CHANNELS;
+  deviceConfig.sampleRate = DEVICE_SAMPLE_RATE;
+  deviceConfig.dataCallback = data_callback;
+
+  if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+    printf("Failed to open playback device.\n");
+    return -4;
+  }
+
+  printf("Device Name: %s\n", device.playback.name);
+
+  if (ma_device_start(&device) != MA_SUCCESS) {
+    printf("Failed to start playback device.\n");
+    ma_device_uninit(&device);
+    return -5;
+  }
+
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(main_loop__em, 0, 1);
+#else
+  printf("Press Enter to quit...\n");
+  getchar();
+#endif
+
+  ma_device_uninit(&device);
+
+  (void)argc;
+  (void)argv;
+  return 0;
+}
