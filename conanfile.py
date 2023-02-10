09d104ec91572e6b8be9e5940e21fe0d30f5563f
GarondEisenfaust <chr.schr@web.de>
2023-02-10 16:57:30 +0100

    fixes build

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class CMakeTemplateConan(ConanFile):
 
   def requirements(self):
     self.requires("gtest/1.8.1", private=True, override=False)
-    self.requires("imgui/1.88", private=True, override=False)
+    self.requires("imgui/1.87", private=True, override=False)
     self.requires("glfw/3.3.8", private=True, override=False)
     self.requires("glew/2.2.0", private=True, override=False)
     self.requires("fmt/9.1.0", private=True, override=False)
@@ -21,7 +21,7 @@ class CMakeTemplateConan(ConanFile):
     pass
 
   def imports(self):
-    source = self.deps_cpp_info["imgui"].resdirs[0] + "/bindings/"
+    source = "./res/bindings/"
     destination = "bindings"
 
     self.copy("imgui_impl_glfw.h", dst=destination, src=source)
