108ad27d9cd691b23a2299c6dcae594f5a7d689e
GarondEisenfaust <chr.schr@web.de>
2024-03-05 20:59:30 +0100

    extracts function

--- nes-lib/src/Oscilator.cpp
+++ nes-lib/src/Oscilator.cpp
@@ -6,19 +6,20 @@ Oscilator::Oscilator() {}
 
 Oscilator::~Oscilator() {}
 
+double ApproximateSine(double t) {
+  double j = t * 0.15915;
+  j = j - std::floor(j);
+  return 20.785 * j * (j - 0.5) * (j - 1.0);
+}
+
 double Oscilator::Sample(double t) {
   double a = 0;
   double b = 0;
   double p = dutycycle * 2.0 * M_PI;
-  auto approxsin = [](double t) {
-    double j = t * 0.15915;
-    j = j - (int)j;
-    return 20.785 * j * (j - 0.5) * (j - 1.0);
-  };
   for (double n = 1; n < harmonics; n++) {
     double c = n * frequency * 2.0 * M_PI * t;
-    a += -approxsin(c) / n;
-    b += -approxsin(c - p * n) / n;
+    a += -ApproximateSine(c) / n;
+    b += -ApproximateSine(c - p * n) / n;
   }
 
   return (2.0 * amplitude / M_PI) * (a - b);
